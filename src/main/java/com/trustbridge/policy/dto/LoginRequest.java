9aa04e4cfdd37c0e3146b20b43926312763dcc0a
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-05-05 15:22:52 +0530

    Added Authorization
    * Adding claim file inside domain
    
    * Adding claim evidence code inside domain
    
    * Adding claim history file inside domian
    
    * Adding ClaimStatus file
    
    * Adding KYC document file
    
    * Adding Notification file
    
    * Adding Policy file
    
    * Adding PremiumMultipliers file
    
    * Adding Product file
    
    * Adding User file
    
    * Add ClaimCards component
    
    * Add EmptyState component
    
    * Add Footer component
    
    * Add Form component
    
    * Add PageTitle component
    
    * Add PolicyCard component
    
    * Add ProductCard component
    
    * Add Stat component
    
    * Adding Auth resonse
    
    * Adding login req code
    
    * Adding auth service
    
    * Adding Auth controller

---
+++ src/main/java/com/trustbridge/policy/dto/LoginRequest.java
@@ -0,0 +1,7 @@
+package com.trustbridge.policy.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(@Email String email, @NotBlank String password) {
+}
